f398e8623cfd17616e16d4ff1f961bde785d6751
Francisco Suarez Bastidas <91593664+SuaDev09@users.noreply.github.com>
2025-08-24 11:12:58 -0700

    refactor layout components and enhance sidebar structure

--- src/app/shared/components/layout/sidebar/sidebar.component.ts
+++ src/app/shared/components/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-
+import { PanelModule } from 'primeng/panel';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, PanelModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
